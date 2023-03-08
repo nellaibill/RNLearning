768d6ac65e88f944088db0d0146fda95fef394d2
Saleem <nellaibill@gmail.com>
2023-03-08 17:59:59 +0530

    Added : react-native-firebase and Read Data from Firebase

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,14 +1,36 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ActivityIndicator, FlatList, SafeAreaView, StyleSheet, TextInput, Text } from 'react-native';
 import { Button } from "@react-native-material/core";
 import { realtimeDbConfig } from '../firebase/realtimeDbConfig'
 import { firestoreDB } from '../firebase/firestoreConfig';
 import { query, ref, set } from "firebase/database";
 import { collection, doc, setDoc, addDocs, addDoc } from "firebase/firestore";
+import firestore from '@react-native-firebase/firestore';
 
 const AddProduct = () => {
     const [text, onChangeText] = React.useState('');
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const subscriber = firestore()
+            .collection('users')
+            .onSnapshot(querySnapshot => {
+                const users = [];
 
+                querySnapshot.forEach(documentSnapshot => {
+                    users.push({
+                        ...documentSnapshot.data(),
+                        key: documentSnapshot.id,
+                    });
+                });
+                console.log(users);
+                setUsers(users);
+                setLoading(false);
+            });
+
+        // Unsubscribe from events when no longer in use
+        return () => subscriber();
+    }, []);
     const addProductToFireStore = () => {
         addDoc(collection(firestoreDB, "users"), {
             username: text
@@ -27,6 +49,9 @@ const AddProduct = () => {
             alert('error');
         });
     }
+    if (loading) {
+        return <ActivityIndicator />;
+      }
     return (
         <SafeAreaView>
             <TextInput
@@ -36,6 +61,14 @@ const AddProduct = () => {
             />
             <Button variant="outlined" title="AddFireStore"
                 onPress={() => addProductToFireStore()} />
+            <FlatList
+                data={users}
+                renderItem={({ item }) => (
+                    <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                        <Text>User Name: {item.username}</Text>
+                    </View>
+                )}
+            />
         </SafeAreaView>
     );
 };
