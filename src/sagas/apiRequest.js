7945eb75b7189ad7a39db7279f92d45c5afb6e78
Saleem <nellaibill@gmail.com>
2023-04-30 10:52:26 +0530

    Display data based on error message

--- src/sagas/apiRequest.js
+++ src/sagas/apiRequest.js
@@ -1,16 +1,17 @@
 import { call, put } from 'redux-saga/effects';
 import { setMoviesError } from "../features/movieSlice";
 
-function* handleApiError(error) {
-  yield put(setMoviesError(error.message));
+function* handleApiError(message) {
+  yield put(setMoviesError(message));
 }
 
 export function* apiRequest(requestFunction, ...args) {
+  const requestName = requestFunction?.name
   try {
     const response = yield call(requestFunction, ...args);
     return response;
   } catch (error) {
-    yield call(handleApiError, error);
+    yield call(handleApiError, requestName + "_" + error?.message);
     throw error;
   }
 }
--- src/screens/MovieDetails.js
+++ src/screens/MovieDetails.js
@@ -5,12 +5,12 @@ import { useSelector } from 'react-redux'
 const MovieDetails = () => {
   const { movie, isError, errorMessage
   } = useSelector((state) => ({ ...state.movie }));
-  if (movie.Year === "2002") {
+  if (movie.Year === "2004") {  // This id condition written intentionally to check the App Crashed message for Error boundary class
     throw new Error('Crashed!!!!');
   }
   return (
     <View style={styles.container}>
-      {isError ? <Text>{errorMessage}</Text> :
+      {isError && movie ? <Text>{errorMessage}</Text> :
         <View >
           <Text>Title {movie.Title}</Text>
           <Text>Year {movie.Year}</Text>
@@ -23,20 +23,20 @@ const MovieDetails = () => {
           }
         </View>
       }
-      </View>
+    </View>
 
-      )
+  )
 }
-      const styles = StyleSheet.create({
-        container: {
-        padding: 5,
-      margin: 15
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    margin: 15
   },
-      logo: {
-        margin: 10,
-      width: 250,
-      height: 300,
+  logo: {
+    margin: 10,
+    width: 250,
+    height: 300,
   },
 });
 
-      export default MovieDetails
+export default MovieDetails
